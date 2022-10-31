dccb402497df2c3e1c5eb50dd1b25762c89f91b3
JulianEZT <julianezt3030@gmail.com>
2022-10-31 18:53:59 -0500

    accesibility and responsive

--- src/app/components/accesibility/accesibility.component.ts
+++ src/app/components/accesibility/accesibility.component.ts
@@ -6,7 +6,24 @@ import { AccesibilityService } from "./accesibility.service";
     styleUrls: ['accesibility.component.sass'],
     templateUrl: 'accesibility.component.html'
 })
-export class AccesibilityComponent{
-    constructor(private acessibilityService: AccesibilityService){
+export class AccesibilityComponent {
+    size = 1;
+
+    constructor(private acessibilityService: AccesibilityService) { }
+
+    increaseSize() {
+        const root = document.getElementById('root')
+        if (root && this.size < 1.4) {
+            this.size = this.size + 0.1;
+            root.style.fontSize = this.size + 'vw'
+        }
+    }
+
+    decreaseSize() {
+        const root = document.getElementById('root')
+        if (root && this.size > 0.6) {
+            this.size = this.size - 0.1;
+            root.style.fontSize = this.size + 'vw'
+        }
     }
 }
